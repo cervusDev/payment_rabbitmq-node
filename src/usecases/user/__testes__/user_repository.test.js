c99d598abea87dd74d290dacba1a002cb0955d24
Gustavo Henrique de Almeida Cervera <gustavo.cervera@macroplan.com.br>
2025-03-24 23:07:06 -0300

    refactor tests and create .https

--- src/usecases/user/__testes__/user_repository.test.js
+++ src/usecases/user/__testes__/user_repository.test.js
@@ -1,6 +1,4 @@
 import { UserRepository } from '../repository/user_repository.js';
-import bcrypt from 'bcryptjs';
-import jsonwebtoken from 'jsonwebtoken';
 
 describe('UserRepository', () => {
   let userRepository;
@@ -11,7 +9,7 @@ describe('UserRepository', () => {
 
   describe('findByEmail', () => {
     it('deve retornar o usuário quando o e-mail for encontrado', async () => {
-      const mockUser = { id: 18, email: 'gustavo.cervus@gmail.com', name: 'Gustavo' };
+      const mockUser = { id: 8, email: 'gustavo.cervus@gmail.com', name: 'Gustavo' };
       const result = await userRepository.findByEmailToTest({ email: mockUser.email });
 
       expect(result).toEqual(mockUser);
@@ -26,7 +24,7 @@ describe('UserRepository', () => {
 
   describe('findByUserId', () => {
     it('deve retornar o usuário quando o ID for encontrado', async () => {
-      const mockUser = { id: 18, email: 'gustavo.cervus@gmail.com', name: 'Gustavo' };
+      const mockUser = { id: 8, email: 'gustavo.cervus@gmail.com', name: 'Gustavo' };
       const result = await userRepository.findByUserId({ id: mockUser.id });
 
       expect(result).toEqual(mockUser);
@@ -44,7 +42,7 @@ describe('UserRepository', () => {
       const userId = users[users.length - 1].id;
       
       const userData = { name: 'User', email: `user${userId + 1}@example.com`, password: 'password' };
-      const createdUser = { name: 'User', email: `user${userId + 1}@example.com` };
+      const createdUser = { id: userId + 1, name: 'User', email: `user${userId + 1}@example.com` };
 
       const result = await userRepository.createUser(userData);
       expect(result).toEqual(createdUser);
